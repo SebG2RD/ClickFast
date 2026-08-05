0a87f5d20f3ef4363978355b7ba08e81f30859aa
SebG2RD <gerardsebastien@live.fr>
2026-08-05 10:47:14 +0200

    test: Suppression du faux secret au commit suivant

--- config-exemple.js
+++
@@ -1,5 +0,0 @@
-// Fichier de demonstration : chaines aleatoires, aucun service reel derriere.
-const api_key = "Zq7Kd2LmXv9PbR4tNw8sEy3CjH6uAf1gTiVo5MnB";
-const AWS_ACCESS_KEY_ID = "AKIA4TQ7WZKX9RJM2LPD";
-const AWS_SECRET_ACCESS_KEY = "hK3vN8pQ2mZx7Lw9TbR4cY6uF1sJ5aD0gE2iX8oV";
-module.exports = { api_key, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY };
